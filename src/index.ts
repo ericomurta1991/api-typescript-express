05bd234f9c434c095ebe44b6118d333a82c14892
ericomurta1991 <erico_murta@yahoo.com.br>
2023-08-22 19:35:36 -0300

    projeto atualizado

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import express, {Request, Response, NextFunction} from 'express';
+import userRoute from './routes/users.routes';
+import statusRoute from './routes/status.routes';
 
 const app = express();
+//middleware responsavel por interpretar content-Type, e se for json, tratar o arquivo.
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}));
+
+//configuracoes de Rotas
+app.use(userRoute);
+app.use(statusRoute);
 
 app.get('/status',(req: Request, res: Response, next: NextFunction) => {
     res.status(200).send({ foo: 'bar'});
 });
 
+// Inicializacao do Servidor 
 app.listen(3000, () => {
-    console.log('Aplicacao executando na porta 3000!');
+    console.log('Aplicacao executando na porta 3000! Funcionando');
 });
